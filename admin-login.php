aee31eb90b7a88d59d35818f8cada5c6c4f2a796
Baramee Maprom <BarameeMaprom@gmail.com>
2025-10-13 15:27:26 +0700

    หน้า admin dashboard

--- admin-login.php
+++ admin-login.php
@@ -21,8 +21,25 @@ session_start();
     $admin_email = $_SESSION['admin_email']; 
     $admin_phone = $_SESSION['admin_phone'];
     $admin_picture = $_SESSION['admin_picture'];
+
+    $db = mysqli_connect('localhost','root','','petshop');
+
+    $result_customer = mysqli_query($db, "SELECT COUNT(*) AS total_customer FROM customer");
+    $row_customer = mysqli_fetch_assoc($result_customer);
+    $total_customer = $row_customer['total_customer'];
+
+    
+    $result_product = mysqli_query($db, "SELECT COUNT(*) AS total_product FROM product");
+    $row_product = mysqli_fetch_assoc($result_product);
+    $total_product = $row_product['total_product'];
+
+    
+    $result_sales = mysqli_query($db, "SELECT COUNT(*) AS total_sales FROM orders");
+    $row_sales = mysqli_fetch_assoc($result_sales);
+    $total_sales = $row_sales['total_sales'];
 ?>
 <body>
+    <div class="container-bd">
     <nav>
         <ul>
             <a href="">
@@ -45,7 +62,90 @@ session_start();
             </a>
         </ul>
     </nav>
+    </div>
 
+    <div class="dashboard-bd">
+    <div class="body-content">
+    <h1>Dashboard</h1>
+    <div class="container-eiei">
+        <div class="static-box">
+            <div class="static">
+                <div class="icon">
+                    <i class="fa-solid fa-users-gear"></i>
+            </div>
+            <div class="data">
+                    <h3><?php echo $total_customer; ?></h3>
+                    <span>จำนวนลูกค้า</span>
+                </div>
+            </div>
+
+            <div class="static">
+                <div class="icon">
+                    <i class="fa-solid fa-box-open"></i>
+                </div>
+            <div class="data">
+                    <h3><?php echo $total_product; ?></h3>
+                    <span>จำนวนสินค้า</span>
+                </div>
+            </div>
+
+            <div class="static">
+                <div class="icon">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
+                <div class="data">
+                    <h3><?php echo $total_sales; ?></h3>
+                    <span>ยอดขายทั้งหมด</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <div class="container-data">
+        <h1>รายละเอียดการสั่งซื้อ</h1>
+        <hr>
+        <div class="table-data">
+            <table>
+                <thead>
+                    <tr>
+                        <th>รหัสรายละเอียดออเดอร์</th> <!-- id_detail -->
+                        <th>ชื่อสินค้า</th> <!-- product_name -->
+                        <th>ราคา</th> <!-- price -->
+                        <th>ชื่อผู้ซื้อ</th> <!-- cus_username -->
+                        <th>อีเมล</th> <!-- cus_email -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php // show data by fetch from database
+                            require('conn.php');
+                            $sql = "SELECT OD.order_detail_id, O.order_id, P.product_name, P.price, C.cus_fname, C.cus_lname, C.cus_email  FROM order_detail as OD
+                            join orders as O on OD.order_id = O.order_id
+                            join product as P on OD.product_id = P.product_id
+                            join customer as C on O.cus_id = C.cus_id"; 
+                            $result = mysqli_query($conn, $sql);
+
+
+                                if ($result->num_rows > 0) {
+                                    while ($row = $result->fetch_assoc()) {
+                                        echo "<tr>";
+                                        echo "<td>".$row['order_detail_id']."</td>";
+                                        echo "<td>".htmlspecialchars($row['product_name'])."</td>";
+                                        echo "<td>".htmlspecialchars($row['price'])."</td>";
+                                        echo "<td>".htmlspecialchars($row['cus_fname'].' '.$row['cus_lname'])."</td>";
+                                        echo "<td>".htmlspecialchars($row['cus_email'])."</td>";
+                                        echo "</tr>";
+                                    }
+                                }else {
+                                    echo "0 results";
+                                }
+                            $conn->close();
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
     
 </body>
 </html>
